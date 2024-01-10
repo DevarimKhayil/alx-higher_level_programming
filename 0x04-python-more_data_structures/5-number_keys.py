33696091ca13c4dd729444e810b45ce10bf3e5b2
devarim <wachirabriangeita@gmail.com>
2024-01-10 15:09:48 +0300

    Number of keys

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def number_keys(a_dictionary):
+    # Initialize a counter to store the number of keys
+    count = 0
+
+    # Iterate through the keys in the dictionary and increment the count for each key
+    for key in a_dictionary:
+        count += 1
+
+    return count
