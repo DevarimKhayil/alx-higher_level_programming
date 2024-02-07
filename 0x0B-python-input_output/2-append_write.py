d14deb76cd629f039c2a996403654c748e7bb2db
devarim <wachirabriangeita@gmail.com>
2024-02-07 11:45:47 +0300

    w

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def append_write(filename="", text=""):
+    """
+    Appends a string at the end of a text file (UTF8) and returns the number of characters added.
+
+    Args:
+        filename (str): The name of the file.
+        text (str): The string to append to the file.
+
+    Returns:
+        int: The number of characters added.
+    """
+    with open(filename, mode='a', encoding='utf-8') as file:
+        nb_characters_added = file.write(text)
+    return nb_characters_added
