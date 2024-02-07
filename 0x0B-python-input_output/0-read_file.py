b7ba7ef3ddf61837403bcee1cc5ed074c73cdf93
devarim <wachirabriangeita@gmail.com>
2024-02-07 10:22:44 +0300

    0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Module for reading a text file and printing its content to stdout.
+"""
+
+
+def read_file(filename=""):
+    """
+    Reads a text file (UTF8) and prints its content to stdout.
+
+    Args:
+        filename (str): The name of the file to be read.
+
+    Returns:
+        None
+    """
+    with open(filename, encoding='utf-8') as file:
+        content = file.read()
+        print(content)
