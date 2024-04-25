de490f38249a6d0ed3e74cb0e98a830c3a7c8875
devarim <wachirabriangeita@gmail.com>
2024-04-25 16:48:50 +0300

    2

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# Script to fetch the size of the response body from a given URL.
-curl -s "$1" -o response_body.txt -w '%{size_download}\n' && rm response_body.txt
+# This script displays the size of the body of the response from a given URL
+curl -s "$1" | wc -c
 
